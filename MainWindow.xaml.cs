c830ab551f6bc1486b6f981afc2d8a3adbb06c63
FurkanZekiTurkzeybek <furkan.fztr@gmail.com>
2023-05-27 00:16:52 +0300

    -V1 Started -Week 10 presentation

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Runtime.InteropServices.JavaScript;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,86 +16,144 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Newtonsoft.Json;
 
-namespace WpfApp1
-{
+
+namespace WpfApp1 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
-    {
+    public class MyPerson {
+        public String latestState;
+        public String name;
+        public String surname;
+        public String address;
+
+        public MyPerson(string latestState, string name, string surname, string address) {
+            this.latestState = latestState;
+            this.name = name;
+            this.surname = surname;
+            this.address = address;
+        }
+    }
+
+    public partial class MainWindow : Window {
         private bool _distress = false;
         private bool _aid = false;
         private bool _safe = false;
 
-        public MainWindow()
-        {
+        public MainWindow() {
             InitializeComponent();
         }
 
-        private void setDistress()
-        {
+        private void setDistress() {
             _distress = true;
             _aid = false;
             _safe = false;
         }
 
-        private void setAid()
-        {
+        private void setAid() {
             _distress = false;
             _aid = true;
             _safe = false;
         }
 
-        private void setSafe()
-        {
+        private void setSafe() {
             _distress = false;
             _aid = false;
             _safe = true;
         }
 
-        private int getStatus()
-        {
-            if (_distress == true && _aid == false && _safe == false)
-            {
+        private int getStatus() {
+            if (_distress == true && _aid == false && _safe == false) {
                 return 1;
             }
-            else if (_distress == false && _aid == true && _safe == false)
-            {
+            else if (_distress == false && _aid == true && _safe == false) {
                 return 2;
             }
-            else if (_distress == false && _aid == false && _safe == true)
-            {
+            else if (_distress == false && _aid == false && _safe == true) {
                 return 3;
             }
-            else
-            {
+            else {
                 return 0;
             }
-               
         }
 
-        private void distressedSelected(object sender, RoutedEventArgs e)
-        {
+        private void distressedSelected(object sender, RoutedEventArgs e) {
             setDistress();
         }
 
-        private void aidSelected(object sender, RoutedEventArgs e)
-        {
+        private void aidSelected(object sender, RoutedEventArgs e) {
             setAid();
         }
 
-        private void safeSelected(object sender, RoutedEventArgs e)
-        {
+        private void safeSelected(object sender, RoutedEventArgs e) {
             setSafe();
         }
 
-        private void listClicked(object sender, RoutedEventArgs e)
-        {
+        private HttpClient client = new HttpClient();
+
+        private String url = "http://localhost:3000/myFunction?param1=value1&param2=value2";
+
+        private void listClicked(object sender, RoutedEventArgs e) {
             int statusVariable = getStatus();
+
+
             //send this command to the JS code and execute the searching according to that
             //after js search is done and print the resulting hashes and information to the window
-            outputList.Items.Add(statusVariable);
+            test(statusVariable);
+            string filePath = @"D:\Programming\SourceCodes\_BC_read\DesktopApp\read\output.json";
+
+            try {
+                string json = File.ReadAllText(filePath);
+                // List<string> jsonDataList = new List<string> { json };
+                // outputList.ItemsSource = jsonDataList;
+
+                // List<String> dataList = JsonConvert.DeserializeObject<List<String>>(json);
+
+
+                List<MyPerson> jsonDataList = JsonConvert.DeserializeObject<List<MyPerson>>(json);
+                // MyPerson outputPerson = jsonDataList[0];
+                // MessageBox.Show(outputPerson.name);
+
+                // MyPerson outputPerson = JsonConvert.DeserializeObject<MyPerson>(json);
+                // string pName = outputPerson.name;
+                // outputList.ItemsSource = pName;
+
+
+                foreach (MyPerson person in jsonDataList) {
+                    outputList.Items.Add(person.name + " || " + person.surname + " || " + person.address);
+                    // outputList.Items.Add(person.surname); 
+                }
+            }
+            catch (Exception exception) {
+                // MessageBox.Show("Error:" + exception.Message);
+                throw;
+            }
+        }
+
+        private async void test(int pStatVariable) {
+            HttpClient client = new HttpClient();
+            String url = "http://localhost:3000/read";
+
+
+            String statusStringToSend = pStatVariable.ToString();
+            var data = new Dictionary<String, String> {
+                { "statusType", statusStringToSend }
+            };
+            var queryString = new FormUrlEncodedContent(data);
+
+            url += "?" + queryString.ReadAsStringAsync().Result; //?
+            HttpResponseMessage response = await client.PostAsync(url, queryString);
+
+            if (response.IsSuccessStatusCode) {
+                String result = await response.Content.ReadAsStringAsync();
+                // MessageBox.Show(result);
+            }
+
+            else {
+                MessageBox.Show("Cannot fetch the data");
+            }
         }
     }
 }
